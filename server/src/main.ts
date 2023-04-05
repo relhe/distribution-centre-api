412de1a7153ee482afd7acf0a5dfc6abcc00d34a
relhe <renellherisson@gmail.com>
2023-04-05 17:56:33 -0400

    added swagger api

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,28 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { json, urlencoded } from 'express';
 
-async function bootstrap() {
+const bootstrap = async () => {
     const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api');
+    app.useGlobalPipes(new ValidationPipe());
+    app.enableCors();
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ limit: '50mb', extended: true }));
+    const config = new DocumentBuilder()
+        .setTitle('Server Framework')
+        .setDescription(
+            'Server Framework API description for distribution centre',
+        )
+        .setVersion('1.0.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document);
+    SwaggerModule.setup('', app, document);
+
     await app.listen(3000);
-}
+};
+
 bootstrap();

6f1c0ec2229fbe3bb3b3403eee61f017eb4f4552
relhe <renellherisson@gmail.com>
2023-04-05 19:37:02 -0400

    completed client controller

--- server/src/controllers/clients/clients.controller.ts
+++ server/src/controllers/clients/clients.controller.ts
@@ -3,40 +3,90 @@ import {
     Get,
     Post,
     Body,
-    Patch,
     Param,
     Delete,
+    Put,
 } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { CreateClientDto } from './model/dto/create-client.dto';
 import { UpdateClientDto } from './model/dto/update-client.dto';
+import {
+    ApiConflictResponse,
+    ApiCreatedResponse,
+    ApiNoContentResponse,
+    ApiNotFoundResponse,
+    ApiOkResponse,
+    ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Clients')
 @Controller('clients')
 export class ClientsController {
     constructor(private readonly clientsService: ClientsService) {}
 
-    @Post()
-    create(@Body() createClientDto: CreateClientDto) {
-        return this.clientsService.create(createClientDto);
-    }
-
+    @ApiOkResponse({
+        description: 'The records have been successfully retrieved.',
+    })
+    @ApiNotFoundResponse({
+        description: 'The records were not found.',
+    })
     @Get()
     findAll() {
         return this.clientsService.findAll();
     }
 
+    @ApiCreatedResponse({
+        description: 'The record has been successfully created.',
+    })
+    @ApiConflictResponse({
+        description: 'The record already exists.',
+    })
+    @Post(/*'create'*/)
+    create(@Body() createClientDto: CreateClientDto) {
+        return this.clientsService.create(createClientDto);
+    }
+
+    @ApiOkResponse({
+        description: 'The record has been successfully retrieved.',
+    })
+    @ApiNotFoundResponse({
+        description: 'The record was not found.',
+    })
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return this.clientsService.findOne(+id);
+        return this.clientsService.findOne(id);
     }
 
-    @Patch(':id')
+    @ApiOkResponse({
+        description: 'The record has been successfully updated.',
+    })
+    @ApiNotFoundResponse({
+        description: 'The record was not found.',
+    })
+    @Put(':id')
     update(@Param('id') id: string, @Body() updateClientDto: UpdateClientDto) {
-        return this.clientsService.update(+id, updateClientDto);
+        return this.clientsService.update(id, updateClientDto);
     }
 
+    @ApiOkResponse({
+        description: 'The record has been successfully deleted.',
+    })
+    @ApiNoContentResponse({
+        description: 'The record was not found.',
+    })
     @Delete(':id')
     remove(@Param('id') id: string) {
-        return this.clientsService.remove(+id);
+        return this.clientsService.remove(id);
+    }
+
+    @ApiOkResponse({
+        description: 'The records have been successfully deleted.',
+    })
+    @ApiNoContentResponse({
+        description: 'The records were not found.',
+    })
+    @Delete()
+    removeAll() {
+        return this.clientsService.removeAll();
     }
 }

5035bd70c057653a3f916bd1f2f74e520c34ddc7
relhe <renellherisson@gmail.com>
2023-04-05 17:51:04 -0400

    added connection to db -- setup controller

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule } from './controllers/clients/clients.module';
-import { ClientsController } from './controllers/clients/clients.controller';
-import { ClientsService } from './controllers/clients/clients.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
@@ -16,9 +14,6 @@ import { MongooseModule } from '@nestjs/mongoose';
                 uri: config.get<string>('DB_CONNECTION_URI'),
             }),
         }),
-        MongooseModule.forFeature([]),
     ],
-    controllers: [ClientsController],
-    providers: [ClientsService],
 })
 export class AppModule {}

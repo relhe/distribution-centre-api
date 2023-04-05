b6e29d887a7145047460685767331b479665c587
relhe <renellherisson@gmail.com>
2023-04-05 18:49:32 -0400

    add swagger enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { json, urlencoded } from 'express';
 
 const bootstrap = async () => {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, { cors: true });
     app.setGlobalPrefix('api');
     app.useGlobalPipes(new ValidationPipe());
     app.enableCors();

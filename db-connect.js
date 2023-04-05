47f3ed18e95bed9c6b55236d19daa09491c25032
relhe <renellherisson@gmail.com>
2023-04-05 19:49:55 -0400

    update project files

--- db-connect.js
+++ db-connect.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'DISTRIBUTION',
-    password: 'postgres',
-    port: 5432,
+    user: 'DB_USR',
+    host: 'DB_HOST',
+    database: 'DB_NAME',
+    password: 'DB_PWD',
+    port: 'DB_PORT' || 5432,
 });
 
 module.exports = pool;

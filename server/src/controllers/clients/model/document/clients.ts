3228f3b3f7028b41f861c3d703fd799dc8934d94
relhe <renellherisson@gmail.com>
2023-04-05 19:38:30 -0400

    added DTO and schema

--- server/src/controllers/clients/model/document/clients.ts
+++ server/src/controllers/clients/model/document/clients.ts
@@ -1,31 +1,25 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
-import { Document } from 'mongoose';
+import { Document, HydratedDocument } from 'mongoose';
 
-export type ClientsDocument = Clients & Document;
+export type ClientsDocument = HydratedDocument<Client>;
 @Schema()
-export class Clients {
-    @ApiProperty({
-        example: '5f1f3e9c9a1a1a0b0d8e8f9c',
-        description: 'Unique identifier',
-    })
-    _id: string;
-
+export class Client {
     @ApiProperty({ example: 'John', description: 'Client name' })
-    @Prop()
+    @Prop({ required: true })
     name: string;
 
     @ApiProperty({ example: '', description: 'Client email' })
-    @Prop()
+    @Prop({ required: true })
     email: string;
 
     @ApiProperty({ example: '1234567890', description: 'Client phone' })
-    @Prop()
+    @Prop({ required: true })
     phone: string;
 
     @ApiProperty({ example: '123 Main St', description: 'Client address' })
-    @Prop()
+    @Prop({ required: true })
     address: string;
 }
 
-export const clientsSchema = SchemaFactory.createForClass(Clients);
+export const clientSchema = SchemaFactory.createForClass(Client);
--- server/src/controllers/clients/model/dto/create-client.dto.ts
+++ server/src/controllers/clients/model/dto/create-client.dto.ts
@@ -1,18 +1,23 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateClientDto {
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     name: string;
 
-    @IsString()
-    @IsNotEmpty()
+    @ApiProperty()
+    @ApiProperty()
+    @IsEmail()
     email: string;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     phone: string;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     address: string;

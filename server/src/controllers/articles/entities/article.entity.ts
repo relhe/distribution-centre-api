ebc96761e9380240ae7adb03b60b985df1a4f327
relhe <renellherisson@gmail.com>
2023-04-07 20:23:54 -0400

    created articles crud

---
+++ server/src/controllers/articles/entities/article.entity.ts
@@ -0,0 +1,11 @@
+export class Article {
+    id: string;
+    name: string;
+    category: string;
+    manufacturer: string;
+    origin: string;
+    weight: number;
+    dimension: string;
+    price: number;
+    units: number;
+}

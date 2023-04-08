56f237fadb8a46f84bce2d375797e0c0485a45c0
relhe <renellherisson@gmail.com>
2023-04-07 20:26:49 -0400

    updated client schema

--- server/src/controllers/clients/model/document/client.ts
+++ server/src/controllers/clients/model/document/client.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
-import { Document, HydratedDocument } from 'mongoose';
+import { HydratedDocument } from 'mongoose';
 
 export type ClientsDocument = HydratedDocument<Client>;
 @Schema()
